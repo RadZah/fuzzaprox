347819eb18f365b532a3c07cfa27c86c0f09327a
RadZah <23409885+RadZah@users.noreply.github.com>
2026-01-19 13:09:53 +0100

    just docstrings to data classes for better clarity on their purpose

--- fuzzaprox/services/data_approx_classes.py
+++ fuzzaprox/services/data_approx_classes.py
@@ -3,6 +3,7 @@ import numpy as np
 
 @dataclass(frozen=True)
 class InputData:
+    """INPUT DATA"""
     x: np.ndarray
     input_y: np.ndarray
     normalized_y: np.ndarray
@@ -10,6 +11,7 @@ class InputData:
 
 @dataclass(frozen=True)
 class ApproxResults:
+    """RESULTS of approximations - both FW and INV"""
     x: np.ndarray
     upper_y: np.ndarray
     bottom_y: np.ndarray
@@ -17,6 +19,7 @@ class ApproxResults:
 
 @dataclass(frozen=True)
 class FuzzaproxResult:
+    """FINAL RETURN Data Class"""
     input_data: InputData
     forward: ApproxResults
     inverse: ApproxResults
